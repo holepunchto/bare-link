167537eda7c2966724565249ddf4ec74f9e2a8dc
yasser <dev@yasser.email>
2025-03-13 09:45:02 -0300

    Provide prebuilt `patchelf` binaries (#1)
    Co-authored-by: Kasper Isager Dalsgarð <kasperisager@hey.com>

---
+++ cmake/ports/patchelf/port.cmake
@@ -0,0 +1,31 @@
+include_guard(GLOBAL)
+
+if(WIN32)
+  set(bin patchelf.exe)
+else()
+  set(bin patchelf)
+endif()
+
+declare_port(
+  "https://github.com/NixOS/patchelf/releases/download/0.18.0/patchelf-0.18.0.tar.gz"
+  patchelf
+  AUTOTOOLS
+  BYPRODUCTS bin/${bin}
+  ARGS
+    --host=${CMAKE_HOST_SYSTEM_PROCESSOR}-${CMAKE_HOST_SYSTEM_NAME}
+  ENV
+    "CC=${CMAKE_C_COMPILER}"
+    "CFLAGS=--target=${CMAKE_C_COMPILER_TARGET}"
+    "CXX=${CMAKE_CXX_COMPILER}"
+    "CXXFLAGS=--target=${CMAKE_CXX_COMPILER_TARGET}"
+)
+
+add_executable(patchelf IMPORTED GLOBAL)
+
+add_dependencies(patchelf ${patchelf})
+
+set_target_properties(
+  patchelf
+  PROPERTIES
+  IMPORTED_LOCATION "${patchelf_PREFIX}/bin/${bin}"
+)
