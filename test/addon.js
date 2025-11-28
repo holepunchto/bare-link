a3428cad9a2eddd7c8320f037a236bd9eca6bf1e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-28 10:00:31 +0100

    Try loading linked addons

--- test/addon.js
+++ test/addon.js
@@ -1,7 +1,7 @@
 const test = require('brittle')
 const path = require('path')
 const link = require('..')
-const { paths } = require('./helpers')
+const { paths, tryLoadAddon } = require('./helpers')
 
 const fixtures = path.resolve(__dirname, 'fixtures')
 
@@ -24,6 +24,12 @@ test('addon, darwin-arm64', async (t) => {
       'addon.1.2.3.framework'
     ])
   )
+
+  const addon = tryLoadAddon(path.join(out, 'addon.1.2.3.framework/Versions/A/addon.1.2.3'), [
+    'darwin-arm64'
+  ])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
 })
 
 test('addon, darwin-arm64 + darwin-x64', async (t) => {
@@ -45,6 +51,13 @@ test('addon, darwin-arm64 + darwin-x64', async (t) => {
       'addon.1.2.3.framework'
     ])
   )
+
+  const addon = tryLoadAddon(path.join(out, 'addon.1.2.3.framework/Versions/A/addon.1.2.3'), [
+    'darwin-arm64',
+    'darwin-x64'
+  ])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
 })
 
 test('addon, ios-arm64', async (t) => {
@@ -167,6 +180,28 @@ test('addon, linux-arm64', async (t) => {
   }
 
   t.alike(result, paths(['lib/libaddon.1.2.3.so']))
+
+  const addon = tryLoadAddon(path.join(out, 'lib/libaddon.1.2.3.so'), ['linux-arm64'])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
+})
+
+test('addon, linux-x64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of await link(path.join(fixtures, 'addon'), {
+    out,
+    target: ['linux-x64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(result, paths(['lib/libaddon.1.2.3.so']))
+
+  const addon = tryLoadAddon(path.join(out, 'lib/libaddon.1.2.3.so'), ['linux-x64'])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
 })
 
 test('addon, linux-arm64 + linux-x64', async (t) => {
@@ -195,6 +230,28 @@ test('addon, win32-arm64', async (t) => {
   }
 
   t.alike(result, paths(['addon-1.2.3.dll']))
+
+  const addon = tryLoadAddon(path.join(out, 'addon-1.2.3.dll'), ['win32-arm64'])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
+})
+
+test('addon, win32-x64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of await link(path.join(fixtures, 'addon'), {
+    out,
+    target: ['win32-x64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(result, paths(['addon-1.2.3.dll']))
+
+  const addon = tryLoadAddon(path.join(out, 'addon-1.2.3.dll'), ['win32-x64'])
+
+  if (addon) t.alike(addon.exports, 'Hello from addon')
 })
 
 test('addon, win32-arm64 + win32-x64', async (t) => {
--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,14 @@
 const path = require('path')
+const { pathToFileURL } = require('url')
 
 exports.paths = function paths(list) {
   return list.map(path.normalize)
+}
+
+exports.tryLoadAddon = function tryLoadAddon(path, hosts = []) {
+  if (typeof Bare === 'undefined' || !hosts.includes(Bare.Addon.host)) {
+    return null
+  }
+
+  return Bare.Addon.load(pathToFileURL(path))
 }
